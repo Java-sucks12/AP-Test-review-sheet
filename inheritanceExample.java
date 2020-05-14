0373254b59d84ca8c4fc1d565041eb2464da06b3
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2020-05-14 10:34:58 -0500

    Create inheritanceExample.java

---
+++ inheritanceExample.java
@@ -0,0 +1,13 @@
+//This is going to be the child class of human.
+//inheritance is used when wanting to create something specific but needs to be similar to another class.
+//use "extends" to inherit
+
+public class Citizen extends Human{
+    
+    String occupation;
+    double pay;
+    
+    public Citizen(String 
+    
+    
+}
