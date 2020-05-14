39e03090769b3291638decc03a220fef037af363
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2020-05-14 11:16:04 -0500

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,18 @@
+//arrays. will not be creating a class for this one
+
+String[] arr = {"Hello", "Hola", "Ni hao"};
+//iteration
+for (int i=0; i< arr.length; i++){
+    System.out.println(arr[i]);
+}
+
+//adding to array and declaring storage before creating array
+int x = 10;
+int[x] arrNum;
+
+for (int i=0; i<x-1; i++){
+  arrNum[i] = i;
+}
+
+//pretty simple.
+
