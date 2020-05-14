d729ade8d7d3341fcac28bfab7c5a58851bca5b9
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2020-05-14 10:48:13 -0500

    Create Loops.java
    iteration will go along with arrays. There will be basic examples of embedded loops in here that hopefully isn't on the test.

---
+++ Loops.java
@@ -0,0 +1,50 @@
+//this class is going to be all about loops
+//make sure you can understand classes a little bit before you read
+
+public class Loops{
+    
+    int count;
+    int arr;
+    
+    public Loops(){
+      count = 0;
+    }
+    
+    public void forLoop(){
+      //basic for loop that counts to 10
+      //please always start at 0 unless there are certain situations. (will explain later)
+      
+      for (int i=0; i<11; i++){
+          System.out.println(i);
+      }
+      
+      //loop that counts backwards
+      
+      for (int i=10; i>=0; i--){
+          System.out.println(i);
+      }
+      
+      
+    }
+    
+    public void whileLoop(){
+        //while loop that counts to 10
+        
+        int i = 0;
+        
+        while (i <= 10){
+          System.out.println(i);
+          i++;
+        }
+        
+        //while loop that counts backwards
+        i = 10;
+        
+        while (i >= 0){
+          System.out.println(i)
+          i--;
+        }
+        
+    }
+
+}
