2d134dd3a846fdfbe51ec3f5dcc01e77cab56ab3
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2020-05-14 12:17:04 -0500

    Update selectionSort.java

--- Algorithms/selectionSort.java
+++ Algorithms/selectionSort.java
@@ -1,3 +1,22 @@
 //again this was in one of our lessons.
 //They will most likely ask you to do this on an array but if they ask about a data structure pass it to an array, sort than convert back.
+//this is something you just copy and paste.
 
+
+class SelectionSort {
+  static void SelectionSort(int arr[]){
+    for (int k=0; k < arr.length-1; k++){
+     int min = k;
+      for (int j = k+1; j < arr.length; j++){
+        if (arr[j] < arr[min]){
+          min = j;
+        }
+        int temp = arr[min];
+        arr[min] = arr[k];
+        arr[k] = temp;
+        
+      }
+    }
+  }
+  
+}
