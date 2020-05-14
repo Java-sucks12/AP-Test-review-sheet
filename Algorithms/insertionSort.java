3e26ecea54e3e87ae26f4b9fafb6cc33f8baf22e
Java-sucks12 <56374609+Java-sucks12@users.noreply.github.com>
2020-05-14 12:06:05 -0500

    Update insertionSort.java

--- Algorithms/insertionSort.java
+++ Algorithms/insertionSort.java
@@ -1,1 +1,19 @@
+//this was actually in one of our lessons.
+//if there is something wrong tell me.
 
+class InsertionSort{
+ static void InsertionSort(int arr[]){
+   int j;
+   for (int i=1; i<arr.length; i++){
+    j = 1;
+     while (j > 0 && arr[j-1] > arr[j]){
+      int temp = arr[j];
+       arr[j] = arr[j-1];
+       arr[j-1] = temp;
+       j--
+     }
+     
+   }
+   
+ }
+}
